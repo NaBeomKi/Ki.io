b226efdd65afc4861980037c45b7c38242335ac5
beomki69 <beomki69@gmail.com>
2021-07-19 13:50:50 +0900

    feat: Add global styles and theme

---
+++ gatsby-browser.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import Theme from "./src/themes/theme";
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  * {
+    box-sizing: border-box;
+    word-break: keep-all;
+  }
+  body {
+    font-family: ${(props) =>
+      props.theme.fonts
+        .main}, --apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+`;
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+    {element}
+  </ThemeProvider>
+);
