fdf9251ded3fadfa91693521f94cc42b78d6a17c
beomki69 <beomki69@gmail.com>
2021-07-30 21:50:00 +0900

    feat: Migration from Jekyll to Gatsby

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  title: "blog title",
-  description: "blog description",
-  author: "yourname",
-  introduction: `Hello, Nice to meet you. I made this blog!`,
+  title: "Ki's Blog",
+  description: "Ki's Tech Blog",
+  author: "Ki",
+  introduction: `I want to become a developer.`,
   siteUrl: `https://gatsby-ki-blog-demo.netlify.app`,
   image: "/images/thumbnail.png",
-  email: "youremail@gmail.com",
+  email: "beomki69@gmail.com",
   social: {
-    twitter: `your.twitter`, // Your Twitter account
-    github: `https://github.com/beomki69/gatsby-ki-blog`, // Your GitHub account
-    facebook: `your.facebook`, // Your Facebook account
-    linkedin: `your.linkedin`, // Your LinkedIn account
-    instagram: `your.instagram`, // Your Instagram account
+    twitter: ``, // Your Twitter account
+    github: `https://github.com/beomki69`, // Your GitHub account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
   },
   icon: `src/images/logo.png`, // Add your favicon
   keywords: ["gatsby blog", "gatsby MDX blog"],
   comments: {
-    utterancesRepo: "beomki69/gatsby-ki-blog", // Your repository for archive comment
+    utterancesRepo: "beomki69/blog-comments", // Your repository for archive comment
   },
-  googleAnalyticsId: `0`, // Add your google analytics tranking ID
+  googleAnalyticsId: `G-QX0RH1R50D`, // Add your google analytics tranking ID
 };
