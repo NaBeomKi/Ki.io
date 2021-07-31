0f6ccca12228f72f7d31023d98c9382a1ee79fa5
beomki69 <beomki69@gmail.com>
2021-07-31 21:49:35 +0900

    Revert "feat: Add gatsby-plugin-sitemap"
    This reverts commit 3a355f1ce79035daf9b34e1513b50370389da546.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,26 +6,6 @@ module.exports = {
     "gatsby-plugin-styled-components",
     "gatsby-plugin-image",
     {
-      resolve: "gatsby-plugin-sitemap",
-      options: {
-        output: `/sitemap.xml`,
-        query: `
-        {
-          site {
-            siteMetadata {
-              siteUrl
-            }
-          }
-          allSitePage {
-            nodes {
-              path
-            }
-          }
-        }
-        `,
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
