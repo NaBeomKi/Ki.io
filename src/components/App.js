e672afced33581a0bc06cd5e6fa5021f4dc58d15
nabeomki <beomki69@gmail.com>
2021-12-10 20:32:08 +0900

    fix: Metadata 보이지 않는 문제 수정

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,13 @@
 import React, { memo } from "react";
-import { useTheme } from "../store/StoreContext";
 import { Seo, Header, Footer } from "./index";
 
 export const App = memo(({ children, title, imgUrl, excerpt }) => {
-  const theme = useTheme();
   return (
-    theme && (
-      <>
-        <Seo title={title} image={imgUrl} description={excerpt} />
-        <Header />
-        {children}
-        <Footer />
-      </>
-    )
+    <>
+      <Seo title={title} image={imgUrl} description={excerpt} />
+      <Header />
+      {children}
+      <Footer />
+    </>
   );
 });
